600f5888b97675c62d8ce89fb2af38ac44ce222e
Arkreuz <coder@arkreuz.com>
2017-06-02 05:32:54 -0500

    Enviroment variables

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ indexRoutes =  require("./routes/index");
 //mongoose.connect("mongodb://localhost/yelpcamp");
 mongoose.connect("mongodb://Arkreuz:Arkeyword@ds161041.mlab.com:61041/yelpcampv2");
 //   mongodb://Arkreuz:Arkeyword@ds161041.mlab.com:61041/yelpcampv2
+
+//console.log(process.env.DATABASEURL);
+mongoose.connect(DATABASEURL);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
